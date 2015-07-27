fb76e43bc864851a44df7c8e2b9da950b5087056
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2015-07-27 19:38:07 +0000

    Initial commit with the bare fixins.
    This might be the starter project place for new Python projects.

---
+++ manifests/vagrant.pp
@@ -0,0 +1,16 @@
+node 'vagrant-trusty64' {
+
+    package { 'pip':
+        ensure   => latest,
+        provider => 'pip',
+        require  => Package['python-pip'],
+    }
+
+    package { 'python-pip':
+        ensure => present
+    }
+
+    package { ['python-dev', 'build-essential']:
+        ensure => present
+    }
+}
