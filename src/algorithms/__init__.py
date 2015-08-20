e5b87ca4ce0b31b084e5bbc73aab5c56cf3cfca9
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2015-08-20 10:51:53 -0700

    Test algorithm and math assumptions.

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
-    
+
 def swap(collection, index1, index2):
     """
-    Swaps the value of the item at index1 in the collection with the value of 
+    Swaps the value of the item at index1 in the collection with the value of
     index2 in the collection.
     """
     # get elements at positions
     first, second = collection[index1], collection[index2]
-    
+
     # swap their positions
     collection[index1] = second
     collection[index2] = first
@@ -18,7 +18,7 @@ def bubble_sort(collection):
     """
     Uses the bubble sort algorithm to sort a collection.
     """
-    # save time if you can
+    # save time if you can, Ω(1)
     if len(collection) <= 1:
         return
 
@@ -38,7 +38,7 @@ def bubble_sort(collection):
             if collection[i] > collection[j]:
                 swap(collection, i, j)
                 touched_something = True
- 
+
 
 def insertion_sort(collection):
     """
@@ -47,15 +47,15 @@ def insertion_sort(collection):
     # save time if you can
     if len(collection) <= 1:
         return
-    
+
     # iterate from the second to the last elements in this array
     for i in range(1, len(collection)): # second parameter is non-inclusive
         j = i
-        
-        # for each master loop, search downward, provided that the previous 
+
+        # for each master loop, search downward, provided that the previous
         while j > 0 and collection[j - 1] > collection[j]:
             # j-1 is greater than j because of loop condition
             swap(collection, j - 1, j)
-            
+
             # decrement j
             j = j - 1
--- src/algorithms/tests.py
+++ src/algorithms/tests.py
@@ -43,12 +43,12 @@ class AlgorithmsTestCase(unittest.TestCase):
 
         self.assertEqual([i for i in xrange(0, 10)], worst)
 
-    
+
     def test_insertion_sort(self):
         """
         Tests the insertion sort algorithm implementation.
         """
-        
+
         collection = [5, 1, 10, 2, 16, 1]
 
         algorithms.insertion_sort(collection)
@@ -62,3 +62,58 @@ class AlgorithmsTestCase(unittest.TestCase):
 
         self.assertEqual([i for i in xrange(0, 10)], collection)
 
+
+    def test_assumption_crawl_forward(self):
+        """
+        Tests assumptions about the actual step requirements for an algorithm which nests loops.
+
+        This test concerns cases like this:
+
+            for i in xrange(0, n):
+                for j in xrange(i + 1, n):
+                    o(1)
+
+        How many iterations total will be run for this equation? Conventional knowledge tells us that the mathematical
+        formula for deriving this is:
+
+            n * (n - 1)
+            ———————————
+                 2
+
+        Or, in Python:
+
+            (n * (n - 1)) / 2.0
+
+        This test validates that assumption in code.
+        """
+        # define an iterate up approach
+        def iterate_up(n):
+            """
+            For every value up to n (non-inclusive) as i, iterate upward from i + 1 to n (non-inclusive).
+            """
+            count = 0
+
+            for i in xrange(0, n):
+                for j in xrange(i + 1, n):
+                    count += 1
+
+            return count
+
+        # define an iterate down approach, like insertion sort
+        def iterate_down(n):
+            """
+            For every value up to n (non-inclusive) as i, iterate downward from i - 1 to 0 (inclusive).
+            """
+            count = 0
+
+            for i in xrange(0, n):
+                for j in reversed(xrange(0, i)): # remember that xrange is NON-INCLUSIVE, therefore we're going to i - 1
+                    count += 1
+
+            return count
+
+        self.assertEqual((100 * (100 - 1)) / 2.0, iterate_up(100))
+        self.assertEqual((1000 * (1000 - 1)) / 2.0, iterate_up(1000))
+
+        self.assertEqual((100 * (100 - 1)) / 2.0, iterate_down(100))
+        self.assertEqual((1000 * (1000 - 1)) / 2.0, iterate_down(1000))
